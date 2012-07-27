4d7dc1f258d94fb94ae473b483eb5a638b2ae119
Suren A. Chilingaryan <csa@dside.dyndns.org>
2012-07-27 13:54:32 +0200

    Bail out from ipecamera_get_next_event if camera is stopped during the wait

--- ipecamera/events.c
+++ ipecamera/events.c
@@ -123,17 +123,18 @@ int ipecamera_next_event(pcilib_context_t *vctx, pcilib_timeout_t timeout, pcili
 		pcilib_calc_deadline(&tv, timeout);
 
 #ifdef IPECAMERA_ANNOUNCE_READY
-		while ((pcilib_calc_time_to_deadline(&tv) > 0)&&(((!ctx->preproc)&&(ctx->reported_id == ctx->event_id))||((ctx->preproc)&&(ctx->reported_id == ctx->preproc_id)))) {
+		while ((ctx->started)&&(pcilib_calc_time_to_deadline(&tv) > 0)&&(((!ctx->preproc)&&(ctx->reported_id == ctx->event_id))||((ctx->preproc)&&(ctx->reported_id == ctx->preproc_id)))) {
 #else /* IPECAMERA_ANNOUNCE_READY */
-		while ((pcilib_calc_time_to_deadline(&tv) > 0)&&(ctx->reported_id == ctx->event_id)) {
+		while ((ctx->started)&&(pcilib_calc_time_to_deadline(&tv) > 0)&&(ctx->reported_id == ctx->event_id)) {
 #endif /* IPECAMERA_ANNOUNCE_READY */
-		usleep(IPECAMERA_NOFRAME_SLEEP);
+		    usleep(IPECAMERA_NOFRAME_SLEEP);
 		}
 	    }
-	    
 	}
 	
-	if (ctx->reported_id == ctx->event_id) return PCILIB_ERROR_TIMEOUT;
+	if (ctx->reported_id == ctx->event_id) {
+	    return PCILIB_ERROR_TIMEOUT;
+	}
 	
     }
 
