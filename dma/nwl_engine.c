21b136f8538df45f2fc0b0ef100a5eaeacf5ac9e
Suren A. Chilingaryan <csa@dside.dyndns.org>
2012-10-29 19:20:28 +0100

    Do not return DMA buffer in streaming read if the callback returns error

--- dma/nwl_engine.c
+++ dma/nwl_engine.c
@@ -299,10 +299,10 @@ int dma_nwl_stream_read(pcilib_dma_context_t *vctx, pcilib_dma_engine_t dma, uin
 	pcilib_kmem_sync_block(ctx->pcilib, info->pages, PCILIB_KMEM_SYNC_FROMDEVICE, bufnum);
         void *buf = pcilib_kmem_get_block_ua(ctx->pcilib, info->pages, bufnum);
 	ret = cb(cbattr, (eop?PCILIB_DMA_FLAG_EOP:0), bufsize, buf);
+	if (ret < 0) return -ret;
 //	DS: Fixme, it looks like we can avoid calling this for the sake of performance
 //	pcilib_kmem_sync_block(ctx->pcilib, info->pages, PCILIB_KMEM_SYNC_TODEVICE, bufnum);
 	dma_nwl_return_buffer(ctx, info);
-	if (ret < 0) return -ret;
 	
 	res += bufsize;
 
