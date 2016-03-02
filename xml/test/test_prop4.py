1e53d2d17e611586030e3ff3e9ab87e5bb753c9b
Suren A. Chilingaryan <csa@suren.me>
2016-03-03 00:10:09 +0100

    Fix CMakeLists.txt script for Python3

--- xml/test/test_prop4.py
+++ xml/test/test_prop4.py
@@ -10,7 +10,6 @@ def read_from_register(ctx, value):
       read_from_register.counter += 1
       for i in range (0, 5):
          time.sleep(0.1)
-         print(cur)
       out = ctx.get_property('/test/prop3') / 2
 
       ctx.unlock('test_prop4')
